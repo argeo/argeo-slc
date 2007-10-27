65b673fc73112c65788ad8863c9c7be36766d737
Mathieu Baudier <mbaudier@argeo.org>
2007-10-27 21:16:27 +0000

    Add test result part
    git-svn-id: https://svn.argeo.org/slc/trunk@664 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.slc/src/main/java/org/argeo/slc/core/test/TestResult.java
+++ org.argeo.slc/src/main/java/org/argeo/slc/core/test/TestResult.java
@@ -2,5 +2,5 @@ package org.argeo.slc.core.test;
 
 /** The result of a test */
 public interface TestResult {
-	public TestResultId geTestResultId();
+	public TestResultId getTestResultId();
 }
---
+++ org.argeo.slc/src/main/java/org/argeo/slc/core/test/TestResultListener.java
@@ -0,0 +1,5 @@
+package org.argeo.slc.core.test;
+
+public interface TestResultListener {
+	public void resultPartAdded(TestResultPart testResultPart);
+}
