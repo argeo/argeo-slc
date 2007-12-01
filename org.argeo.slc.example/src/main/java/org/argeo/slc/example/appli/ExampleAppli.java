fb6d4fddbaebc10487a1082f2d59c15c716ea455
Mathieu Baudier <mbaudier@argeo.org>
2007-12-01 23:03:16 +0000

    Introduce SLC example
    git-svn-id: https://svn.argeo.org/slc/trunk@804 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ org.argeo.slc.example/src/main/java/org/argeo/slc/example/appli/ExampleAppli.java
@@ -0,0 +1,48 @@
+package org.argeo.slc.example.appli;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/** Example appli. */
+public class ExampleAppli {
+	private int skipFreq = 2;
+
+	/** Filters. */
+	public void filter(String[] args) {
+		if (args.length < 2) {
+			throw new RuntimeException(
+					"Not enough arguments. Usage: <inpuit file> <output file>");
+		}
+		String input = args[0];
+		String output = args[1];
+		if (args.length > 2) {
+			skipFreq = Integer.parseInt(args[2]);
+		}
+
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(input));
+			FileWriter out = new FileWriter(output);
+			int count = 0;
+			String line;
+			while ((line = in.readLine()) != null) {
+				if (count % skipFreq != 0) {
+					out.write(line);
+					out.write("\n");
+				}
+				count++;
+			}
+			out.close();
+			in.close();
+		} catch (IOException e) {
+			throw new RuntimeException("Appli failed", e);
+		}
+	}
+
+	/** Sets the frequency of the lines to skip. */
+	public void setSkipFreq(int skipFreq) {
+		this.skipFreq = skipFreq;
+	}
+
+}
