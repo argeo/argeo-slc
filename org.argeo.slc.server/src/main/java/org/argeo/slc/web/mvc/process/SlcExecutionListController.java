c3ae2336076d0c1b7b2408133639158cc69f8b12
Mathieu Baudier <mbaudier@argeo.org>
2008-04-22 10:04:14 +0000

    First end-to-end SlcExecution with web service
    git-svn-id: https://svn.argeo.org/slc/trunk@1021 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ org.argeo.slc.server/src/main/java/org/argeo/slc/web/mvc/process/SlcExecutionListController.java
@@ -0,0 +1,41 @@
+package org.argeo.slc.web.mvc.process;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.ParameterizableViewController;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.argeo.slc.core.process.SlcExecution;
+import org.argeo.slc.dao.process.SlcExecutionDao;
+
+public class SlcExecutionListController extends ParameterizableViewController {
+	private Log log = LogFactory.getLog(getClass());
+
+	private final SlcExecutionDao slcExecutionDao;
+
+	public SlcExecutionListController(SlcExecutionDao slcExecutionDao) {
+		this.slcExecutionDao = slcExecutionDao;
+	}
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		ModelAndView modelAndView = new ModelAndView();
+
+		List<SlcExecution> slcExecutions = slcExecutionDao.listSlcExecutions();
+		if (log.isDebugEnabled()) {
+			log.debug("SlcExecutions (" + slcExecutions.getClass() + "): "
+					+ slcExecutions);
+		}
+		modelAndView.addObject("slcExecutions", slcExecutions);
+		modelAndView.setViewName(getViewName());
+		return modelAndView;
+	}
+
+}
--- org.argeo.slc.server/src/main/java/org/argeo/slc/ws/process/SlcExecutionRequestEp.java
+++ org.argeo.slc.server/src/main/java/org/argeo/slc/ws/process/SlcExecutionRequestEp.java
@@ -3,20 +3,26 @@ package org.argeo.slc.ws.process;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.argeo.slc.core.process.SlcExecution;
+import org.argeo.slc.dao.process.SlcExecutionDao;
 import org.argeo.slc.msg.process.SlcExecutionRequest;
 import org.springframework.ws.server.endpoint.AbstractMarshallingPayloadEndpoint;
 
-public class SlcExecutionRequestEp extends
-		AbstractMarshallingPayloadEndpoint {
+public class SlcExecutionRequestEp extends AbstractMarshallingPayloadEndpoint {
 
 	private Log log = LogFactory.getLog(getClass());
+
+	private final SlcExecutionDao slcExecutionDao;
+
+	public SlcExecutionRequestEp(SlcExecutionDao slcExecutionDao) {
+		this.slcExecutionDao = slcExecutionDao;
+	}
 
 	@Override
 	protected Object invokeInternal(Object requestObject) throws Exception {
 		SlcExecutionRequest msg = (SlcExecutionRequest) requestObject;
 		SlcExecution slcExecution = msg.getSlcExecution();
-		log.info("Received save or update request fro SlcExecution "
-				+ slcExecution.getUuid());
+		slcExecutionDao.create(slcExecution);
+		log.debug("Created SlcExecution with uuid " + slcExecution.getUuid());
 		return null;
 	}
 
