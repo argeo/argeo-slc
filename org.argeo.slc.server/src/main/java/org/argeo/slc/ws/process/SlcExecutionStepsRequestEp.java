566554259d48596809aafc567ccc845cefdfb905
Mathieu Baudier <mbaudier@argeo.org>
2008-04-24 11:15:48 +0000

    Fix bugs with slc execution notifications
    git-svn-id: https://svn.argeo.org/slc/trunk@1048 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.slc.server/src/main/java/org/argeo/slc/ws/process/SlcExecutionStepsRequestEp.java
+++ org.argeo.slc.server/src/main/java/org/argeo/slc/ws/process/SlcExecutionStepsRequestEp.java
@@ -1,5 +1,7 @@
 package org.argeo.slc.ws.process;
 
+import java.util.List;
+
 import org.springframework.ws.server.endpoint.AbstractMarshallingPayloadEndpoint;
 
 import org.apache.commons.logging.Log;
@@ -7,6 +9,7 @@ import org.apache.commons.logging.LogFactory;
 
 import org.argeo.slc.core.SlcException;
 import org.argeo.slc.core.process.SlcExecution;
+import org.argeo.slc.core.process.SlcExecutionStep;
 import org.argeo.slc.dao.process.SlcExecutionDao;
 import org.argeo.slc.msg.process.SlcExecutionStepsRequest;
 
@@ -32,7 +35,14 @@ public class SlcExecutionStepsRequestEp extends
 			if (slcExecution == null)
 				throw new SlcException("Could not find slc execution " + uuid);
 
-			slcExecution.getSteps().addAll(msg.getSteps());
+			List<SlcExecutionStep> additionalSteps = msg.getSteps();
+			if(log.isTraceEnabled()){
+				log.trace("Trying to add additional steps to slc execution "+uuid+":");
+				for(SlcExecutionStep step: additionalSteps){
+					log.trace("Step "+step.getUuid()+" (in slc execution "+uuid+")");
+				}
+			}
+			slcExecution.getSteps().addAll(additionalSteps);
 
 			slcExecutionDao.update(slcExecution);
 			log.debug("Added " + msg.getSteps().size()
