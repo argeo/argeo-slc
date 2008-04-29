b4121f6a1f27b519059c26919015cffe43a849e9
Gwenael Prié <gwenael.prie@gmail.com>
2008-04-29 19:48:00 +0000

    Implemented a first version of testUpdate.
    git-svn-id: https://svn.argeo.org/slc/trunk@1080 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.slc.core/src/test/java/org/argeo/slc/hibernate/test/tree/TreeTestResultDaoHibernateTest.java
+++ org.argeo.slc.core/src/test/java/org/argeo/slc/hibernate/test/tree/TreeTestResultDaoHibernateTest.java
@@ -2,7 +2,13 @@ package org.argeo.slc.hibernate.test.tree;
 
 import static org.argeo.slc.core.test.tree.TreeTestResultTestUtils.createCompleteTreeTestResult;
 
+import java.util.Date;
+
+import org.argeo.slc.core.structure.SimpleSElement;
+import org.argeo.slc.core.structure.tree.TreeSPath;
+import org.argeo.slc.core.structure.tree.TreeSRegistry;
 import org.argeo.slc.core.test.tree.TreeTestResult;
+import org.argeo.slc.core.test.tree.TreeTestResultTestUtils;
 import org.argeo.slc.dao.test.tree.TreeTestResultDao;
 import org.argeo.slc.unit.AbstractSpringTestCase;
 import org.argeo.slc.unit.test.tree.UnitTestTreeUtil;
@@ -22,6 +28,47 @@ public class TreeTestResultDaoHibernateTest extends AbstractSpringTestCase {
 		UnitTestTreeUtil.assertTreeTestResult(ttr, ttrPersisted);
 	}
 
+	public void testUpdate() {
+		TreeTestResultDao testResultDao = (TreeTestResultDao) getContext()
+				.getBean("testResultDao");
+
+		TreeTestResult ttr = createCompleteTreeTestResult();
+		testResultDao.create(ttr);
+
+		TreeTestResult ttrUpdated = (TreeTestResult) testResultDao
+				.getTestResult(ttr.getTestResultId());
+		
+		// Modifying ttrUpdated
+		
+		/** this closeDate update commented because 
+		 * the assertTreeTestResult will find a unexpected 
+		 * discrepancy in the date.
+		 * ttrUpdated.setCloseDate(new Date()); 
+		 */
+		
+		String pathStr = "/test";
+		TreeSPath path = TreeSPath.parseToCreatePath(pathStr);
+
+		TreeSRegistry registry = new TreeSRegistry();
+		SimpleSElement elem = new SimpleSElement("Unit Test");
+		elem.getTags().put("myTag", "myTagValue");
+		registry.register(path, elem);
+
+		ttrUpdated.notifyCurrentPath(registry, path);
+		
+		ttrUpdated.addResultPart(TreeTestResultTestUtils.createSimpleResultPartPassed());
+		ttrUpdated.addResultPart(TreeTestResultTestUtils.createSimpleResultPartFailed());
+		ttrUpdated.addResultPart(TreeTestResultTestUtils.createSimpleResultPartError());
+		
+		testResultDao.update(ttrUpdated);
+		
+		// comparison of ttrUpdated and ttrRetrieved
+		TreeTestResult ttrRetrieved = (TreeTestResult) testResultDao
+		.getTestResult(ttr.getTestResultId());
+		
+		UnitTestTreeUtil.assertTreeTestResult(ttrRetrieved, ttrUpdated);
+	}
+	
 	@Override
 	protected String getApplicationContextLocation() {
 		return "org/argeo/slc/hibernate/applicationContext.xml";
