e186b2d692d035a8e4a0ac86144600d1d2b80de4
Mathieu Baudier <mbaudier@argeo.org>
2008-05-04 17:18:09 +0000

    Introduce test descriptors
    git-svn-id: https://svn.argeo.org/slc/trunk@1106 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.slc.server/src/main/java/org/argeo/slc/ws/test/tree/ResultPartRequestEp.java
+++ org.argeo.slc.server/src/main/java/org/argeo/slc/ws/test/tree/ResultPartRequestEp.java
@@ -6,8 +6,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.argeo.slc.core.SlcException;
+import org.argeo.slc.core.test.TestRunDescriptor;
 import org.argeo.slc.core.test.tree.PartSubList;
 import org.argeo.slc.core.test.tree.TreeTestResult;
+import org.argeo.slc.dao.test.TestRunDescriptorDao;
 import org.argeo.slc.dao.test.tree.TreeTestResultDao;
 import org.argeo.slc.msg.test.tree.ResultPartRequest;
 
@@ -16,14 +18,26 @@ public class ResultPartRequestEp extends AbstractMarshallingPayloadEndpoint {
 	private Log log = LogFactory.getLog(getClass());
 
 	private final TreeTestResultDao treeTestResultDao;
+	private final TestRunDescriptorDao testRunDescriptorDao;
 
-	public ResultPartRequestEp(TreeTestResultDao treeTestResultDao) {
+	public ResultPartRequestEp(TreeTestResultDao treeTestResultDao,
+			TestRunDescriptorDao testRunDescriptorDao) {
 		this.treeTestResultDao = treeTestResultDao;
+		this.testRunDescriptorDao = testRunDescriptorDao;
 	}
 
 	@Override
 	protected Object invokeInternal(Object requestObject) throws Exception {
 		ResultPartRequest msg = (ResultPartRequest) requestObject;
+
+		TestRunDescriptor testRunDescriptor = msg.getTestRunDescriptor();
+		if (testRunDescriptor != null) {
+			testRunDescriptorDao.saveOrUpdate(testRunDescriptor);
+			if (log.isDebugEnabled())
+				log.debug("Updated test run descriptor with id "
+						+ testRunDescriptor.getTestRunUuid());
+		}
+
 		TreeTestResult treeTestResult = treeTestResultDao.getTestResult(msg
 				.getResultUuid());
 		if (treeTestResult == null) {
