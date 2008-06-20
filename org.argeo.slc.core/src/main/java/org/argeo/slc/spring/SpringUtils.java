80389feff3ab8e8743db4abfb6493285f72b24dd
Mathieu Baudier <mbaudier@argeo.org>
2008-06-20 13:25:18 +0000

    Simplify new runtime
    git-svn-id: https://svn.argeo.org/slc/trunk@1256 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.slc.core/src/main/java/org/argeo/slc/spring/SpringUtils.java
+++ org.argeo.slc.core/src/main/java/org/argeo/slc/spring/SpringUtils.java
@@ -1,10 +1,18 @@
 package org.argeo.slc.spring;
 
+import java.io.IOException;
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.argeo.slc.core.SlcException;
 import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
 
 public class SpringUtils {
+	private final static Log log = LogFactory.getLog(SpringUtils.class);
+
 	public static <T> T loadSingleFromContext(ListableBeanFactory context,
 			Class<T> clss) {
 		Map<String, T> beans = context.getBeansOfType(clss);
@@ -12,6 +20,39 @@ public class SpringUtils {
 			return beans.values().iterator().next();
 		} else {
 			return null;
+		}
+	}
+
+	public static Resource getParent(Resource res) {
+		try {
+			if (res.getURL().getPath().equals("/"))
+				return null;
+
+			String urlStr = res.getURL().toString();
+			if (urlStr.charAt(urlStr.length() - 1) == '/')
+				urlStr = urlStr.substring(0, urlStr.length() - 2);
+
+			String parentUrlStr = urlStr.substring(0, urlStr.lastIndexOf('/'));
+			return new DefaultResourceLoader(Thread.currentThread()
+					.getContextClassLoader()).getResource(parentUrlStr + '/');
+		} catch (IOException e) {
+			throw new SlcException("Cannot get parent for resource " + res, e);
+		}
+	}
+
+	public static String extractRelativePath(Resource ancestor, Resource child) {
+		try {
+			String childPath = child.getURL().getPath();
+			String ancestorPath = ancestor.getURL().getPath();
+
+			if (log.isTraceEnabled())
+				log.trace("extractRelativePath(): childPath=" + childPath
+						+ ", ancestorPath=" + ancestorPath);
+
+			return childPath.substring(ancestorPath.length());
+		} catch (IOException e) {
+			throw new SlcException("Cannot extract relative path of " + child
+					+ " based on " + ancestor, e);
 		}
 	}
 
