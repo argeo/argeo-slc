586f272c28e25edecf61df134238022ca268067a
Mathieu Baudier <mbaudier@argeo.org>
2008-06-21 12:10:05 +0000

    Improve logging
    git-svn-id: https://svn.argeo.org/slc/trunk@1262 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.slc.example/src/test/java/org/argeo/slc/example/ExecutionTest.java
+++ org.argeo.slc.example/src/test/java/org/argeo/slc/example/ExecutionTest.java
@@ -6,12 +6,8 @@ import junit.framework.TestCase;
 
 public class ExecutionTest extends TestCase {
 	public void testSimpleRun() {
-		String[] args = {
-				"--mode",
-				"single",
-				"--script",
-				"/home/mbaudier/workspace/org.argeo.slc.example/exampleSlcAppli/root/Category1/SubCategory2/build.xml",
-				"-p", "log4j.logger.org.argeo.slc=WARN" };
+		String[] args = { "--mode", "single", "--script",
+				"exampleSlcAppli/root/Category1/SubCategory2/build.xml" };
 		SlcMain.main(args);
 	}
 }
