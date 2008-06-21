001355d964b2b932adffbb7d1c1113bb17109a59
Mathieu Baudier <mbaudier@argeo.org>
2008-06-21 13:13:14 +0000

    Introduce runtime selection
    git-svn-id: https://svn.argeo.org/slc/trunk@1264 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.slc.example/src/test/java/org/argeo/slc/example/ExecutionTest.java
+++ org.argeo.slc.example/src/test/java/org/argeo/slc/example/ExecutionTest.java
@@ -7,7 +7,8 @@ import junit.framework.TestCase;
 public class ExecutionTest extends TestCase {
 	public void testSimpleRun() {
 		String[] args = { "--mode", "single", "--script",
-				"exampleSlcAppli/root/Category1/SubCategory2/build.xml" };
+				"exampleSlcAppli/root/Category1/SubCategory2/build.xml",
+				"--runtime", "default" };
 		SlcMain.main(args);
 	}
 }
