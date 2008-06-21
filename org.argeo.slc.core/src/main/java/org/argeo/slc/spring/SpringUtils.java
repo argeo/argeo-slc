fd322e14f9f8fc992d582f375921978bf9c1f8e4
Mathieu Baudier <mbaudier@argeo.org>
2008-06-21 17:51:52 +0000

    Normalize URLs
    git-svn-id: https://svn.argeo.org/slc/trunk@1272 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.slc.core/src/main/java/org/argeo/slc/spring/SpringUtils.java
+++ org.argeo.slc.core/src/main/java/org/argeo/slc/spring/SpringUtils.java
@@ -1,6 +1,8 @@
 package org.argeo.slc.spring;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
@@ -27,23 +29,25 @@ public class SpringUtils {
 		try {
 			if (res.getURL().getPath().equals("/"))
 				return null;
-			
+
 			String urlStr = res.getURL().toString();
 			if (urlStr.charAt(urlStr.length() - 1) == '/')
 				urlStr = urlStr.substring(0, urlStr.length() - 2);
 
-			String parentUrlStr = urlStr.substring(0, urlStr.lastIndexOf('/'));
+			String parentUrlStr = urlStr.substring(0, urlStr.lastIndexOf('/')) + '/';
+			URI uri = new URI(parentUrlStr).normalize();
 			return new DefaultResourceLoader(Thread.currentThread()
-					.getContextClassLoader()).getResource(parentUrlStr + '/');
-		} catch (IOException e) {
+					.getContextClassLoader()).getResource(uri.toString());
+		} catch (Exception e) {
 			throw new SlcException("Cannot get parent for resource " + res, e);
 		}
 	}
 
 	public static String extractRelativePath(Resource ancestor, Resource child) {
 		try {
-			
-			return ancestor.getURI().relativize(child.getURI()).toString();
+
+			return ancestor.getURI().relativize(child.getURI()).normalize()
+					.toString();
 		} catch (IOException e) {
 			throw new SlcException("Cannot extract relative path of " + child
 					+ " based on " + ancestor, e);
