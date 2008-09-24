b9ed0fde35ffd88e7f4d385d72dd5ed7d6ba9ce9
Mathieu Baudier <mbaudier@argeo.org>
2008-09-24 17:20:23 +0000

    Restructure the dependencies between UI projects
    git-svn-id: https://svn.argeo.org/slc/trunk@1628 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ sandbox/argeo.slc.testui/src/test/java/org/argeo/slc/jemmytest/SwingTestJemmy.java
@@ -0,0 +1,39 @@
+package org.argeo.slc.jemmytest;
+
+import junit.framework.TestCase;
+
+import org.argeo.slc.testui.SwingTestUi;
+import org.netbeans.jemmy.ClassReference;
+import org.netbeans.jemmy.operators.JButtonOperator;
+import org.netbeans.jemmy.operators.JFrameOperator;
+import org.netbeans.jemmy.operators.JLabelOperator;
+
+public class SwingTestJemmy extends TestCase {
+//	private final static Log log = LogFactory.getLog(SwingTestJemmy.class);
+
+	public void testSimple() throws Exception {
+		// Start application
+		ClassReference classReference = new ClassReference(SwingTestUi.class
+				.getName());
+		String[] args = { "noExitOnClose" };
+		classReference.startApplication(args);
+
+		// Find components
+		JFrameOperator jFrameOperator = new JFrameOperator("HelloWorldSwing");
+		JButtonOperator jButtonOperator = new JButtonOperator(jFrameOperator,
+				"Button");
+		JLabelOperator jLabelOperator = new JLabelOperator(jFrameOperator,
+				"Hello World");
+
+		// Execute actions
+		jButtonOperator.push();
+
+		// Performs checks
+		String textAfterPush = jLabelOperator.getText();
+		assertEquals("Pressed!!", textAfterPush);
+
+		// Clean up
+		jFrameOperator.close();
+	}
+
+}
