b2f23cc85abe3bc31711c04746b80244b43bd442
Mathieu Baudier <mbaudier@argeo.org>
2008-09-26 08:19:56 +0000

    Use Activator's classloader
    git-svn-id: https://svn.argeo.org/slc/trunk@1638 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- sandbox/argeo.slc.jemmytest/src/main/java/org/argeo/slc/jemmytest/JemmyTestActivator.java
+++ sandbox/argeo.slc.jemmytest/src/main/java/org/argeo/slc/jemmytest/JemmyTestActivator.java
@@ -12,22 +12,22 @@ public class JemmyTestActivator implements BundleActivator {
 	private AbstractApplicationContext applicationContext;
 
 	public void start(BundleContext context) throws Exception {
-		// ClassLoader classLoader = context.getBundle().getClass()
-		// .getClassLoader();
-		//
-		// Thread cur = Thread.currentThread();
-		// ClassLoader save = cur.getContextClassLoader();
-		// cur.setContextClassLoader(classLoader);
-		//
-		// try {
-		// // FIXME: make it more generic
-		// applicationContext = new ClassPathXmlApplicationContext(
-		// "/slc/conf/applicationContext.xml");
-		// } catch (Exception e) {
-		// throw new Exception("Could not initialize application context", e);
-		// } finally {
-		// cur.setContextClassLoader(save);
-		// }
+		ClassLoader classLoader = getClass().getClassLoader();
+
+		Thread cur = Thread.currentThread();
+		ClassLoader save = cur.getContextClassLoader();
+		cur.setContextClassLoader(classLoader);
+
+		try {
+			// FIXME: make it more generic
+			applicationContext = new ClassPathXmlApplicationContext(
+					"/slc/conf/applicationContext.xml");
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new Exception("Could not initialize application context");
+		} finally {
+			cur.setContextClassLoader(save);
+		}
 
 		// applicationContext = new GenericApplicationContext();
 		// XmlBeanDefinitionReader xmlReader = new XmlBeanDefinitionReader(
@@ -43,10 +43,10 @@ public class JemmyTestActivator implements BundleActivator {
 		// }
 
 		Properties properties = new Properties();
-		AutoUiApplicationJemmy applicationJemmy = new AutoUiApplicationJemmy();
-		// AutoUiApplicationJemmy applicationJemmy = (AutoUiApplicationJemmy)
-		// applicationContext
-		// .getBean("jemmyTest");
+		// AutoUiApplicationJemmy applicationJemmy = new
+		// AutoUiApplicationJemmy();
+		AutoUiApplicationJemmy applicationJemmy = (AutoUiApplicationJemmy) applicationContext
+				.getBean("jemmyTest");
 		context.registerService(AutoUiApplication.class.getName(),
 				applicationJemmy, properties);
 		stdOut("JemmyTest started");
