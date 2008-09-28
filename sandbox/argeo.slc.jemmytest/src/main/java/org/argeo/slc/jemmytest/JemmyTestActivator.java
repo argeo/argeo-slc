4bafe4b45c92437f769495c038203a048a4e793a
Mathieu Baudier <mbaudier@argeo.org>
2008-09-28 12:41:43 +0000

    Use SLC Detached (aka. SLC AutoUI) as a bundle. Move servers config to end bundle.
    git-svn-id: https://svn.argeo.org/slc/trunk@1650 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- sandbox/argeo.slc.jemmytest/src/main/java/org/argeo/slc/jemmytest/JemmyTestActivator.java
+++ sandbox/argeo.slc.jemmytest/src/main/java/org/argeo/slc/jemmytest/JemmyTestActivator.java
@@ -1,25 +1,6 @@
 package org.argeo.slc.jemmytest;
 
-import java.util.Properties;
+import org.argeo.slc.autoui.SimpleDetachedActivator;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.argeo.slc.autoui.AbstractDetachedActivator;
-import org.argeo.slc.autoui.DetachedStep;
-import org.osgi.framework.BundleContext;
-
-public class JemmyTestActivator extends AbstractDetachedActivator {
-	private final Log log = LogFactory.getLog(getClass());
-	
-	protected void startAutoBundle(BundleContext context) throws Exception {
-//		Properties properties = new Properties();
-//		DummyStep applicationJemmy = (DummyStep) getStaticRef("jemmyTest");
-//		context.registerService(DetachedStep.class.getName(),
-//				applicationJemmy, properties);
-		log.info("JemmyTest started");
-	}
-
-	public void stopAutoBundle(BundleContext context) throws Exception {
-		log.info("JemmyTest stopped");
-	}
+public class JemmyTestActivator extends SimpleDetachedActivator {
 }
