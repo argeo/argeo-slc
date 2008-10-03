4213f814ad8aa5c1e261e72253b222bbc2668f31
Mathieu Baudier <mbaudier@argeo.org>
2008-10-03 08:44:19 +0000

    Start introducing detached ui
    git-svn-id: https://svn.argeo.org/slc/trunk@1691 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- sandbox/argeo.slc.testui/src/main/java/org/argeo/slc/testui/SwingTestUi.java
+++ sandbox/argeo.slc.testui/src/main/java/org/argeo/slc/testui/SwingTestUi.java
@@ -5,17 +5,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class SwingTestUi {
 	private static void createAndShowGUI(boolean exitOnClose) {
 		// Create and set up the window.
-		JFrame frame = new JFrame("HelloWorldSwing");
+		final JFrame frame = new JFrame("HelloWorldSwing");
 		if (exitOnClose)
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		frame.getContentPane().setLayout(new GridLayout(2, 1));
+		frame.getContentPane().setLayout(new GridLayout(3, 1));
 
 		// Add the ubiquitous "Hello World" label.
 		final JLabel label = new JLabel("Hello World");
@@ -29,6 +30,18 @@ public class SwingTestUi {
 					label.setText("Pressed!!");
 				else
 					label.setText("Hello World");
+			}
+		});
+
+		final JButton buttonStart = new JButton("Start");
+		frame.getContentPane().add(buttonStart);
+		buttonStart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JDialog dialog = new JDialog(frame, "TestDialog");
+				JLabel label = new JLabel("Dialog open");
+				dialog.getContentPane().add(label);
+				dialog.pack();
+				dialog.setVisible(true);
 			}
 		});
 
