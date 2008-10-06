99c664e31f5268fcb420f6ab54afbd6289312d22
Mathieu Baudier <mbaudier@argeo.org>
2008-10-06 08:02:43 +0000

    Allow to configure JVM arguments
    git-svn-id: https://svn.argeo.org/slc/trunk@1717 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.slc.detached.launcher/src/main/base/bin/slc-detached.bat
+++ org.argeo.slc.detached.launcher/src/main/base/bin/slc-detached.bat
@@ -11,7 +11,7 @@ FOR %%f IN (%SLC_HOME%\lib\detached\org.argeo.slc.detached.launcher-*.jar) DO se
 
 set CLASSPATH=%SLC_DETACHED_LAUNCHER_JAR%;%SLC_USER_CLASSPATH%;%SLC_LIB_DETACHED%\aopalliance-1.0.jar;%SLC_LIB_DETACHED%\commons-io-1.4.jar;%SLC_LIB_DETACHED%\commons-logging-1.1.1.jar;%SLC_LIB_DETACHED%\log4j-1.2.15.jar;%SLC_LIB_DETACHED%\org.apache.felix.main-1.2.1.jar;%SLC_LIB_DETACHED%\spring-beans-2.0.8.jar;%SLC_LIB_DETACHED%\spring-context-2.0.8.jar;%SLC_LIB_DETACHED%\spring-core-2.0.8.jar;%SLC_LIB_DETACHED%\xercesImpl-2.8.1.jar;%SLC_LIB_DETACHED%\xml-apis-1.3.03.jar;%SLC_LIB_DETACHED%\xalan-2.7.0.jar
 
-set CMD=%JAVA_HOME%\bin\java -Dslc.home=%SLC_HOME% -Dslc.workDir=%SLC_WORK_DIR% "-Dslc.detached.userBundles=%SLC_USER_BUNDLES%" -Dslc.detached.appclass=%SLC_DETACHED_APPCLASS% "-Dslc.detached.appargs=%SLC_DETACHED_APPARGS%" -classpath %CLASSPATH% org.argeo.slc.detached.launcher.Main
+set CMD=%JAVA_HOME%\bin\java %SLC_DETACHED_JVM_ARGS% -Dslc.home=%SLC_HOME% -Dslc.workDir=%SLC_WORK_DIR% "-Dslc.detached.userBundles=%SLC_USER_BUNDLES%" -Dslc.detached.appclass=%SLC_DETACHED_APPCLASS% "-Dslc.detached.appargs=%SLC_DETACHED_APPARGS%" -classpath %CLASSPATH% org.argeo.slc.detached.launcher.Main
 rem echo CMD=%CMD%
 
 start %CMD%
