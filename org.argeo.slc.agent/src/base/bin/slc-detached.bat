e3eeeeb7b29bd989773a387025e0c2f734f89530
Mathieu Baudier <mbaudier@argeo.org>
2008-10-08 16:39:31 +0000

    Remove forgotten conflicting file
    git-svn-id: https://svn.argeo.org/slc/trunk@1720 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.slc.agent/src/base/bin/slc-detached.bat
+++
@@ -1,9 +0,0 @@
-echo Hello World!
-set SLC_HOME=%~dp0..
-echo SLC_HOME=%SLC_HOME%
-
-set CLASSPATH=
-
-FOR %%f IN (%SLC_HOME%\lib\te*.jar) DO set CLASSPATH=%CLASSPATH%;%%f
-
-echo CLASSPATH=%CLASSPATH%
