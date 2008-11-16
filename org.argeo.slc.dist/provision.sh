71e99443c33cd109d5dfa4c17acbb84ee73c2908
Mathieu Baudier <mbaudier@argeo.org>
2008-11-16 17:00:21 +0000

    Introduce P2 provisioning
    git-svn-id: https://svn.argeo.org/slc/trunk@1826 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.slc.dist/provision.sh
+++ org.argeo.slc.dist/provision.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
-/mnt/wind/dev/tools-linux/eclipse-pde/eclipse -application org.eclipse.equinox.p2.metadata.generator.EclipseGenerator -source /home/mbaudier/dev/src/slc/org.argeo.slc.dist/target/p2Repository/   -metadataRepository file:/home/mbaudier/dev/src/slc/org.argeo.slc.dist/target/p2Repository/   -metadataRepositoryName "Ganymede Update Site"    -artifactRepository file:/home/mbaudier/dev/src/slc/org.argeo.slc.dist/target/p2Repository/   -artifactRepositoryName "Ganymede Artifacts"    -noDefaultIUs   -vmargs -Xmx256m
+ECLIPSE_BASE=/home/mbaudier/lib64/eclipse-3.4
+$ECLIPSE_BASE/eclipse \
+	-application org.eclipse.equinox.p2.metadata.generator.EclipseGenerator \
+	-source ./target/p2Repository/  \
+	-metadataRepository file:/home/mbaudier/dev/work/p2Tests/repository  \
+	-metadataRepositoryName "Argeo SLC Update Site"  \
+	-artifactRepository file:/home/mbaudier/dev/work/p2Tests/repository \
+	-artifactRepositoryName "Argeo SLC Artifacts" \
+	-publishArtifacts \
+	-publishArtifactRepository \
+	-root org.argeo.slc.runtime \
+	-rootVersion 0.11.2.SNAPSHOT \
+	-noDefaultIUs  \
+	-noSplash \
+	-vmargs \
+	-Xmx256m
