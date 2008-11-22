f567ffc9b86811568513a17fd53ad3140a475650
Mathieu Baudier <mbaudier@argeo.org>
2008-11-22 11:25:46 +0000

    Introduce org.argeo.slc.launcher
    git-svn-id: https://svn.argeo.org/slc/trunk@1846 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ runtime/org.argeo.slc.launcher/src/main/java/org/argeo/slc/ant/unit/AntSlcApplicationTestCase.java
@@ -0,0 +1,34 @@
+package org.argeo.slc.ant.unit;
+
+import junit.framework.TestCase;
+
+import org.argeo.slc.ant.AntExecutionContext;
+import org.argeo.slc.cli.DefaultSlcRuntime;
+import org.argeo.slc.runtime.SlcExecutionOutput;
+
+public abstract class AntSlcApplicationTestCase extends TestCase implements
+		SlcExecutionOutput<AntExecutionContext> {
+
+	/** To be overriden */
+	public void postExecution(AntExecutionContext executionContext) {
+
+	}
+
+	protected String getRootDir() {
+		return System.getProperty("slc.rootDir", "src/main/slc/root");
+	}
+
+	protected String getAbsoluteScript(String relative) {
+		return getRootDir() + '/' + relative;
+	}
+
+	protected void execute(String relativeScript) {
+		execute(relativeScript, null);
+	}
+
+	protected void execute(String relativeScript, String targets) {
+		new DefaultSlcRuntime().executeScript(
+				getAbsoluteScript(relativeScript), targets, this);
+	}
+
+}
---
+++ runtime/org.argeo.slc.launcher/src/main/java/org/argeo/slc/maven/MavenDeployEnvironment.java
@@ -0,0 +1,73 @@
+package org.argeo.slc.maven;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.argeo.slc.ant.AntRunner;
+import org.argeo.slc.core.SlcException;
+import org.argeo.slc.core.deploy.DeployEnvironment;
+
+public class MavenDeployEnvironment {/*implements DeployEnvironment {
+	private static final Log log = LogFactory
+			.getLog(MavenDeployEnvironment.class);
+	private MavenManager mavenManager;
+
+	public void unpackTo(Object packg, File targetLocation,
+			Map<String, String> filter) {
+		File packageLocation;
+		String type = null;
+		String removeRootDir = "enabled";
+		if (packg instanceof MavenFile) {
+			packageLocation = mavenManager
+					.getPackageLocation((MavenFile) packg);
+			type = ((MavenFile) packg).getType();
+		} else if (packg instanceof File) {
+			packageLocation = (File) packg;
+			// TODO: type based on extension
+		} else {
+			throw new SlcException("Unrecognized package type "
+					+ packg.getClass());
+		}
+		if (log.isDebugEnabled()) {
+			log.debug("Unpack " + packageLocation + " of type " + type + " to "
+					+ targetLocation);
+		}
+
+		try {
+			File tempDir = new File("/tmp/" + UUID.randomUUID().toString());
+			tempDir.mkdirs();
+			targetLocation.mkdirs();
+			Properties props = new Properties();
+			props.setProperty("dest", targetLocation.getAbsolutePath());
+			props.setProperty("src", packageLocation.getAbsolutePath());
+			props.setProperty("tempDir", tempDir.getAbsolutePath());
+			props.setProperty("removeRootDir", removeRootDir);
+
+			URL antUrl = getClass().getClassLoader().getResource(
+					"org/argeo/slc/support/deploy/ant/build.xml");
+
+			if (type == null || type.equals("zip")) {
+				new AntRunner(antUrl, "deployZip", props).run();
+			} else if (type.equals("tar.gz")) {
+				new AntRunner(antUrl, "deployTarGz", props).run();
+			} else {
+				throw new SlcException("Unknow package type " + type);
+			}
+		} catch (SlcException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new SlcException("Cannot unpack package " + packg + " to "
+					+ targetLocation, e);
+		}
+	}
+
+	public void setMavenManager(MavenManager mavenManager) {
+		this.mavenManager = mavenManager;
+	}
+*/
+}
