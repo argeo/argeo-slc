7b9d4dc58c7b423eae183beff755600f0818a784
Charles du Jeu <charles.dujeu@gmail.com>
2008-12-06 19:23:22 +0000

    Refactoring part II (src move)
    git-svn-id: https://svn.argeo.org/slc/trunk@1901 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ org.argeo.slc.webapp/src/main/webapp/source/class/org/argeo/slc/web/Perspective.js
@@ -0,0 +1,42 @@
+/**
+ * @author Charles
+ */
+qx.Class.define("org.argeo.slc.web.Perspective",
+{
+  extend : qx.core.Object,
+  implement : [org.argeo.ria.components.IPerspective], 
+  
+  construct : function(){
+	  	this.base(arguments);
+  },
+  
+  members : {
+  	  	
+  	initViewPanes : function(viewsManager){
+  		
+      var splitPane = new qx.ui.splitpane.Pane("horizontal");
+	  var topLeft = new org.argeo.ria.components.ViewPane(this, "list", "Collection", {
+	  	orientation : "horizontal",
+	  	min : 36
+	  });
+	  topLeft.set({width:300});
+	  viewsManager.registerViewPane(topLeft);
+	    
+	  splitPane.add(topLeft, 0);
+  	  var rightPane = new org.argeo.ria.components.ViewPane(this, "applet", "Test Detail");  	  
+  	  viewsManager.registerViewPane(rightPane);
+	  splitPane.add(rightPane, 1);
+      
+      viewsManager.getViewPanesContainer().add(splitPane, {flex:1});
+  		
+  	},
+  	
+  	initViews : function(viewsManager){
+	  var view = viewsManager.initIViewClass(org.argeo.slc.web.TestList, "list");
+	  view.load();
+	  var command = org.argeo.ria.event.CommandsManager.getInstance().executeCommand("loadtestlist");
+  	}
+  	
+  }
+
+});
