9f9ffcf0497f9c243490e368b1fa33288cb7bad4
Charles du Jeu <charles.dujeu@gmail.com>
2009-01-09 11:49:00 +0000

    Packages documentations
    git-svn-id: https://svn.argeo.org/slc/trunk@2062 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ org.argeo.slc.webapp/src/main/webapp/argeo-ria-lib/slc/class/org/argeo/slc/ria/__init__.js
@@ -0,0 +1,5 @@
+/**
+ * Package containing SLC generic components to be used by any RIA SLC applications
+ * as a library.
+ * Currently contains the SlcApi client and a canonical Applet for displaying test results.
+ */
