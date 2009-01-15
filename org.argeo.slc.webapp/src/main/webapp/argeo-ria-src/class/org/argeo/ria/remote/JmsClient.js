cc5155c7c9dbc6359043051df64fd4f2bbe28e62
Charles du Jeu <charles.dujeu@gmail.com>
2009-01-15 08:49:58 +0000

    Replace AmqClient by JmsClient. Remove the queue mechanism ported from apache.org _amq script : qooxdoo already provides the RequestQueue mechanism.
    git-svn-id: https://svn.argeo.org/slc/trunk@2099 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- org.argeo.slc.webapp/src/main/webapp/argeo-ria-src/class/org/argeo/ria/remote/JmsClient.js
+++ org.argeo.slc.webapp/src/main/webapp/argeo-ria-src/class/org/argeo/ria/remote/JmsClient.js
@@ -1,62 +1,41 @@
-qx.Class.define("org.argeo.ria.remote.AmqClient", {
+qx.Class.define("org.argeo.ria.remote.JmsClient", {
 
-	extend : qx.core.Object, 
+	type : "singleton",
+	extend : qx.core.Object,
+	
 	construct : function(){
 		this.base(arguments);
 	},
 	members : {
   		// The URI of the MessageListenerServlet
-		uri : '/org.argeo.slc.webapp/amq',
+		uri : '/org.argeo.slc.webapp/amq',		
 
 		// Polling. Set to true (default) if waiting poll for messages is needed
 		poll : true,
+		pollTimeout : 25,
+		interrupt : false,
 
 		// Poll delay. if set to positive integer, this is the time to wait in ms before
 		// sending the next poll after the last completes.
 		_pollDelay : 0,
 
 		_first : true,
-		_pollEvent : function(first) {
-		},
+		_pollEvent : function(first) {},
 		_handlers : new Array(),
-
-		_messages : 0,
-		_messageQueue : '',
-		_queueMessages : 0,
 
 		_messageHandler : function(response) {
 			var doc = response.getContent();
 			var NSMap = {slc:"http://argeo.org/projects/slc/schemas"};
 			var messages = org.argeo.ria.util.Element.selectNodes(doc, "//response", NSMap);
-			//this.info("Received " + messages.length + " messages");
 			for(var i=0;i<messages.length;i++){
 				var id = messages[i].getAttribute("id");
-				var slcExec = org.argeo.ria.util.Element.selectSingleNode(messages[i], "slc:slc-execution", NSMap);				
-				if(id && this._handlers[id] && slcExec){
-					this._handlers[id](slcExec);
+				if(id && this._handlers[id]){
+					this._handlers[id](messages[i]);
 				}
 			}
 		},
-
-		startBatch : function() {
-			this._queueMessages++;
-		},
-
-		endBatch : function() {
-			this._queueMessages--;			
-			if (this._queueMessages == 0 && this._messages > 0) {
-				var body = this._messageQueue;
-				this._messageQueue = '';
-				this._messages = 0;
-				this._queueMessages++;
-				var request = new qx.io.remote.Request(this.uri, "POST", "text/plain");
-				request.addListener("completed", this.endBatch, this);
-				request.send();
-			}
-		},
-
+		
 		_pollHandler : function(response) {
-			this.startBatch();
 			try {
 				this._messageHandler(response);
 				this._pollEvent(this._first);
@@ -64,7 +43,6 @@ qx.Class.define("org.argeo.ria.remote.AmqClient", {
 			} catch (e) {
 				alert(e);
 			}
-			this.endBatch();
 
 			if (this._pollDelay > 0)
 				qx.event.Timer.once(this._sendPoll, this, this._pollDelay);
@@ -73,7 +51,9 @@ qx.Class.define("org.argeo.ria.remote.AmqClient", {
 		},
 
 		_sendPoll : function(request) {
+			if(this.interrupt) return;
 			var request = new qx.io.remote.Request(this.uri, "GET", "application/xml");
+			request.setTimeout(this.pollTimeout*1000+5000);
 			request.addListener("completed", this._pollHandler, this);
 			request.send();
 		},
@@ -108,34 +88,26 @@ qx.Class.define("org.argeo.ria.remote.AmqClient", {
 		},
 
 		_sendMessage : function(destination, message, type) {
-			if (this._queueMessages > 0) {
-				if (this._messages == 0) {
-					this._messageQueue = 'destination=' + destination
-							+ '&message=' + message + '&type=' + type;
-				} else {
-					this._messageQueue += '&d' + this._messages + '='
-							+ destination + '&m' + this._messages + '='
-							+ message + '&t' + this._messages + '=' + type;
-				}
-				this._messages++;
-			} else {
-				this.startBatch();
-				var req = new qx.io.remote.Request(this.uri, "POST", "text/plain");
-				req.setParameter("destination", destination);
-				req.setParameter("message", message);
-				req.setParameter("type", type);
-				req.addListener("completed", this.endBatch, this);
-				req.send();
-			}
+			var req = new qx.io.remote.Request(this.uri, "POST", "text/plain");
+			req.setParameter("destination", destination);
+			req.setParameter("message", message);
+			req.setParameter("type", type);
+			//req.addListener("completed", this.endBatch, this);
+			req.send();
 		},
 
 		startPolling : function() {
 			if (this.poll){
+				this.interrupt = false;
 				var req = new qx.io.remote.Request(this.uri, "GET", "application/xml");
 				req.setParameter("timeout", "10");
 				req.addListener("completed", this._pollHandler, this);
 				req.send();
 			}
-		}		
+		},
+		
+		stopPolling : function(){
+			this.interrupt = true;
+		}
 	}
 });
