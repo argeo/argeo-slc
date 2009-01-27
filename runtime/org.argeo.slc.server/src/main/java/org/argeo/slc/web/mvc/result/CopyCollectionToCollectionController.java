b57f27c766e4ce01990cff3398b9aaf8454ed5d9
Mathieu Baudier <mbaudier@argeo.org>
2009-01-27 11:39:10 +0000

    Add generic copy/remove to/from collection(s)
    git-svn-id: https://svn.argeo.org/slc/trunk@2135 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- runtime/org.argeo.slc.server/src/main/java/org/argeo/slc/web/mvc/result/CopyCollectionToCollectionController.java
+++ runtime/org.argeo.slc.server/src/main/java/org/argeo/slc/web/mvc/result/CopyCollectionToCollectionController.java
@@ -12,12 +12,17 @@ import org.argeo.slc.web.mvc.AbstractServiceController;
 import org.springframework.util.PatternMatchUtils;
 import org.springframework.web.servlet.ModelAndView;
 
-public class RemoveResultFromCollectionController extends
+/**
+ * Copy from a collection to another based on Spring simple pattern matching.
+ * 
+ * @see PatternMatchUtils
+ */
+public class CopyCollectionToCollectionController extends
 		AbstractServiceController {
 	private final TreeTestResultCollectionDao testResultCollectionDao;
 	private final TestManagerService testManagerService;
 
-	public RemoveResultFromCollectionController(
+	public CopyCollectionToCollectionController(
 			TreeTestResultCollectionDao testResultCollectionDao,
 			TestManagerService testManagerService) {
 		this.testResultCollectionDao = testResultCollectionDao;
@@ -28,29 +33,23 @@ public class RemoveResultFromCollectionController extends
 			HttpServletResponse response, ModelAndView modelAndView)
 			throws Exception {
 
-		String collectionId = request.getParameter("collectionId");
-		String[] resultUuids = request.getParameterValues("resultUuid");
+		String sourceCollectionId = request.getParameter("sourceCollectionId");
+		String targetCollectionId = request.getParameter("targetCollectionId");
 		String[] attrNames = request.getParameterValues("attrName");
 		String[] attrPatterns = request.getParameterValues("attrPattern");
 
 		// Checks
-		if (collectionId == null)
-			throw new SlcException("A collection id must be specified");
+		if (sourceCollectionId == null || targetCollectionId == null)
+			throw new SlcException(
+					"Source and target collection ids must be specified");
 		if (attrNames != null
 				&& (attrPatterns == null || attrNames.length != attrPatterns.length))
 			throw new SlcException(
 					"There must be as many attrName as attrPatterns");
 
-		// Remove specified results
-		if (resultUuids != null)
-			for (String resultUuid : resultUuids)
-				testManagerService.removeResultFromCollection(collectionId,
-						resultUuid);
-
+		TreeTestResultCollection sourceCollection = testResultCollectionDao
+				.getTestResultCollection(sourceCollectionId);
 		if (attrNames != null) {
-			TreeTestResultCollection sourceCollection = testResultCollectionDao
-					.getTestResultCollection(collectionId);
-
 			int index = 0;
 			for (String attrName : attrNames) {
 				String attrPattern = attrPatterns[index];// safe: checked above
@@ -59,26 +58,19 @@ public class RemoveResultFromCollectionController extends
 						.getResults()) {
 					if (PatternMatchUtils.simpleMatch(attrPattern,
 							treeTestResult.getAttributes().get(attrName))) {
-						testManagerService.removeResultFromCollection(
-								collectionId, treeTestResult.getUuid());
+						testManagerService.addResultToCollection(
+								targetCollectionId, treeTestResult.getUuid());
 					}
 				}
 				index++;
 			}
 		} else {
-			if (resultUuids == null) {// no specs
-				// remove all
-				// TODO: optimize
-				TreeTestResultCollection sourceCollection = testResultCollectionDao
-						.getTestResultCollection(collectionId);
-				for (TreeTestResult treeTestResult : sourceCollection
-						.getResults()) {
-					testManagerService.removeResultFromCollection(collectionId,
-							treeTestResult.getUuid());
-				}
-
+			// remove all
+			// TODO: optimize
+			for (TreeTestResult treeTestResult : sourceCollection.getResults()) {
+				testManagerService.addResultToCollection(targetCollectionId,
+						treeTestResult.getUuid());
 			}
 		}
-
 	}
 }
