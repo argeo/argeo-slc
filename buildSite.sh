0bc7f579f43741d316d62af5888eef52cc0fb2ca
Mathieu Baudier <mbaudier@argeo.org>
2009-06-06 15:35:37 +0000

    Improve buildscript
    git-svn-id: https://svn.argeo.org/slc/trunk@2496 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- buildSite.sh
+++ buildSite.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
-rm -rf ~/dev/work/slcSite/0.11
-mvn clean install
-mvn site-deploy -Pdeveloper
+#rm -rf ~/dev/work/slcSite/0.11
+
+BUILD_DIR=`pwd`
+HOSTNAME=`hostname -f`
+DATE=`date -u`
+TO=mathieu.baudier@gmail.com
+
+# BUILD
+mvn --fail-at-end deploy
+if [ $? != "0" ] ; then
+	mail -s "Build@$HOSTNAME FAILURE - $DATE - $BUILD_DIR" $TO < pom.xml
+else
+	mail -s "Build@$HOSTNAME SUCCESS - $DATE - $BUILD_DIR" $TO < pom.xml
+fi
+
+# SITE
+mvn --fail-at-end site-deploy
+if [ $? != "0" ] ; then
+	mail -s "SiteGeneration@$HOSTNAME FAILURE - $DATE - $BUILD_DIR" $TO < pom.xml
+else
+	mail -s "SiteGeneration@$HOSTNAME SUCCESS - $DATE - $BUILD_DIR" $TO < pom.xml
+fi
+
