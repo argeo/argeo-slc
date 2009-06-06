42482a0aebe0e054e7416103fd793c81d12e81d9
Mathieu Baudier <mbaudier@argeo.org>
2009-06-06 21:15:20 +0000

    Improve buildscript
    git-svn-id: https://svn.argeo.org/slc/trunk@2501 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- buildSite.sh
+++ buildSite.sh
@@ -1,21 +1,30 @@
 #!/bin/sh
-#rm -rf ~/dev/work/slcSite/0.11
 
+#
+# To be overridden
+#
+TO=mathieu.baudier@gmail.com
+PROFILES=$1
+
+#
+# Code
+#
 BUILD_DIR=`pwd`
 HOSTNAME=`hostname -f`
 DATE=`date -u`
-TO=mathieu.baudier@gmail.com
 
 # BUILD
-mvn --fail-at-end deploy 2>&1 | tee deploy.log
+mvn --fail-at-end deploy -P$PROFILES 2>&1 | tee deploy.log
+
 if [ $? != "0" ] ; then
 	mail -s "Build@$HOSTNAME FAILURE - $DATE - $BUILD_DIR" $TO < deploy.log
 else
-	mail -s "Build@$HOSTNAME SUCCESS - $DATE - $BUILD_DIR" $TO < deploy.log
+	mail -s "Build@$HOSTNAME SUCCESS - $DATE - $BUILD_DIR" $TO < pom.xml
 fi
 
 # SITE
-mvn --fail-at-end site-deploy 2>&1 | tee siteGeneration.log
+mvn --fail-at-end site-deploy -P$PROFILES 2>&1 | tee siteGeneration.log
+
 if [ $? != "0" ] ; then
 	mail -s "SiteGeneration@$HOSTNAME FAILURE - $DATE - $BUILD_DIR" $TO < siteGeneration.log
 else
