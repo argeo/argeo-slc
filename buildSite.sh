5d826965d68f45082429212bcb84d438ce190573
Mathieu Baudier <mbaudier@argeo.org>
2009-06-06 20:34:20 +0000

    Improve buildscript
    git-svn-id: https://svn.argeo.org/slc/trunk@2497 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- buildSite.sh
+++ buildSite.sh
@@ -7,18 +7,18 @@ DATE=`date -u`
 TO=mathieu.baudier@gmail.com
 
 # BUILD
-mvn --fail-at-end deploy
+mvn --fail-at-end deploy 2>&1 | tee deploy.log
 if [ $? != "0" ] ; then
-	mail -s "Build@$HOSTNAME FAILURE - $DATE - $BUILD_DIR" $TO < pom.xml
+	mail -s "Build@$HOSTNAME FAILURE - $DATE - $BUILD_DIR" $TO < deploy.log
 else
-	mail -s "Build@$HOSTNAME SUCCESS - $DATE - $BUILD_DIR" $TO < pom.xml
+	mail -s "Build@$HOSTNAME SUCCESS - $DATE - $BUILD_DIR" $TO < deploy.log
 fi
 
 # SITE
-mvn --fail-at-end site-deploy
+mvn --fail-at-end site-deploy 2>&1 | tee siteGeneration.log
 if [ $? != "0" ] ; then
-	mail -s "SiteGeneration@$HOSTNAME FAILURE - $DATE - $BUILD_DIR" $TO < pom.xml
+	mail -s "SiteGeneration@$HOSTNAME FAILURE - $DATE - $BUILD_DIR" $TO < siteGeneration.log
 else
-	mail -s "SiteGeneration@$HOSTNAME SUCCESS - $DATE - $BUILD_DIR" $TO < pom.xml
+	# No need to send email for successful site generation
 fi
 
