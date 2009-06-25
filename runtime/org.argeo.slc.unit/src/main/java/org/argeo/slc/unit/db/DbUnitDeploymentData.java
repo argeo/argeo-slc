ee6c3543a0ff9403420ce6a9c647723269f14331
Mathieu Baudier <mbaudier@argeo.org>
2009-06-25 17:42:35 +0000

    Restructure modules
    git-svn-id: https://svn.argeo.org/slc/trunk@2583 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ runtime/org.argeo.slc.unit/src/main/java/org/argeo/slc/unit/db/DbUnitDeploymentData.java
@@ -0,0 +1,30 @@
+package org.argeo.slc.unit.db;
+
+import java.io.InputStream;
+
+import org.argeo.slc.SlcException;
+import org.argeo.slc.deploy.DeploymentData;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.springframework.core.io.Resource;
+
+public class DbUnitDeploymentData implements DeploymentData {
+	private Resource dataSetLocation;
+
+	public IDataSet createDataSet() {
+		try {
+			InputStream in = dataSetLocation.getInputStream();
+			IDataSet dataSet = new FlatXmlDataSet(in);
+			in.close();
+			return dataSet;
+		} catch (Exception e) {
+			throw new SlcException("Cannot create data set", e);
+		}
+
+	}
+
+	public void setDataSetLocation(Resource dataSetLocation) {
+		this.dataSetLocation = dataSetLocation;
+	}
+
+}
