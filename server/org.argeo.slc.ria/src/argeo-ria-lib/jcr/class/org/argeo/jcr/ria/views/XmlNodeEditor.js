8aecefe809f016bae788bc009a3f0ae45fb2016b
Charles du Jeu <charles.dujeu@gmail.com>
2010-04-28 08:17:23 +0000

    Simple XML Highlighter
    git-svn-id: https://svn.argeo.org/slc/trunk@3505 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- server/org.argeo.slc.ria/src/argeo-ria-lib/jcr/class/org/argeo/jcr/ria/views/XmlNodeEditor.js
+++ server/org.argeo.slc.ria/src/argeo-ria-lib/jcr/class/org/argeo/jcr/ria/views/XmlNodeEditor.js
@@ -1,5 +1,5 @@
 qx.Class.define("org.argeo.jcr.ria.views.XmlNodeEditor", {
-	extend : qx.ui.embed.Html,
+	extend : qx.ui.container.Composite,
 	implement : [org.argeo.ria.components.IView], 
 
 	properties : {
@@ -26,6 +26,10 @@ qx.Class.define("org.argeo.jcr.ria.views.XmlNodeEditor", {
 	
 	construct : function(){
 		this.base(arguments);
+		this.setLayout(new qx.ui.layout.VBox());
+		this.htmlPane = new qx.ui.embed.Html();
+		this.htmlPane.setOverflow("auto", "auto");
+		this.add(this.htmlPane, {flex:1});
 	},
 	
 	members : {
@@ -38,8 +42,7 @@ qx.Class.define("org.argeo.jcr.ria.views.XmlNodeEditor", {
 		 * @return {Boolean}
 		 */
 		init : function(viewPane, data){
-			this.setViewSelection(new org.argeo.ria.components.ViewSelection(viewPane.getViewId()));	  
-			this.setHtml("Hello!");
+			this.setViewSelection(new org.argeo.ria.components.ViewSelection(viewPane.getViewId()));	  			
 		},
 		/**
 		 * The implementation should contain the real data loading (i.o. query...)
@@ -49,7 +52,32 @@ qx.Class.define("org.argeo.jcr.ria.views.XmlNodeEditor", {
 			rootNode.load();
 			rootNode.addListener("changeLoadState", function(){
 				var xmlString = rootNode.toXmlString(true);
-				this.setHtml(qx.xml.String.escape(xmlString));
+			    var TAG_START_PATTERN = new RegExp("<([0-9a-zA-Z\.]+)([^>]*)>", "gi");
+			    var TAG_END_PATTERN = new RegExp("</([0-9a-zA-Z\.]+)>", "gi");
+			    var TAG_CLOSE_PATTERN = new RegExp("(/?>)", "gi");
+			    var TAG_ATTRIBUTE = new RegExp("\\s([0-9a-zA-Z:]+)\\=\"([^\"]*)\"", "gi");
+			    // Not implemented yet
+			    var TAG_COMMENT = new RegExp("(<!--.*-->)", "gi");
+			    var TAG_CDATA_START = new RegExp("(\\<!\\[CDATA\\[).*", "gi");
+			    var TAG_CDATA_END = new RegExp(".*(]]>)", "gi");
+				
+			    xmlString = xmlString.replace(TAG_START_PATTERN, 'xml_div_begin&lt;xml_tagname_begin$1xml_tagname_end$2&gt;');
+			    xmlString = xmlString.replace(TAG_END_PATTERN, '&lt;/xml_tagname_begin$1xml_tagname_end&gt;xml_div_end');
+			    xmlString = xmlString.replace(TAG_CLOSE_PATTERN, '&gt;');
+			    
+			    xmlString = xmlString.replace(TAG_ATTRIBUTE, ' xml_attname_begin$1xml_attname_end="xml_attvalue_begin$2xml_attvalue_end"');
+				
+				//xmlString = qx.xml.String.escape(xmlString);
+				xmlString = xmlString.replace(new RegExp("(xml_div_begin)", "g"), '<div style="padding-top:2px;padding-left:15px;">');
+				xmlString = xmlString.replace(new RegExp("(xml_div_end)", "g"), '</div>');
+				xmlString = xmlString.replace(new RegExp("(xml_tagname_begin)", "g"), '<b style="color:rgb(63,127,127);">');
+				xmlString = xmlString.replace(new RegExp("(xml_tagname_end)", "g"), '</b>');
+				xmlString = xmlString.replace(new RegExp("(xml_attname_begin)", "g"), '<b>');
+				xmlString = xmlString.replace(new RegExp("(xml_attname_end)", "g"), '</b>');
+				xmlString = xmlString.replace(new RegExp("(xml_attvalue_begin)", "g"), '<span style="color:rgb(0,0,255)">');
+				xmlString = xmlString.replace(new RegExp("(xml_attvalue_end)", "g"), '</span>');
+				xmlString = '<div style="margin-left:-10px;">' + xmlString + '</div>';
+				this.htmlPane.setHtml(xmlString);
 			}, this);
 		},
 		/**
@@ -57,7 +85,7 @@ qx.Class.define("org.argeo.jcr.ria.views.XmlNodeEditor", {
 		 * @return {Boolean}
 		 */
 		addScroll : function(){
-			return true;
+			return false;
 		},
 		/**
 		 * Called at destruction time
