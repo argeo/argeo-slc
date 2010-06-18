00a0697bc8da174a439e2de832b776022a8fc76d
Mathieu Baudier <mbaudier@argeo.org>
2010-06-18 16:56:35 +0000

    Have launch.sh running in the work directory
    git-svn-id: https://svn.argeo.org/slc/trunk@3608 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- runtime/org.argeo.slc.launcher/src/main/base/launch.bat
+++ runtime/org.argeo.slc.launcher/src/main/base/launch.bat
@@ -1,9 +1,10 @@
 @echo off
 set ROOT_DIR=%~dp0
+set LIB_DIR=%ROOT_DIR%\lib
 set WORK_DIR=%ROOT_DIR%\work\%1
 
 TITLE %1
 
 cd %WORK_DIR%
 
-java %JAVA_OPTS% -jar "%ROOT_DIR%\lib\org.eclipse.osgi-${version.equinox}.jar" -clean -console -configuration "%WORK_DIR%\conf" -data "%WORK_DIR%\data"
+java %JAVA_OPTS% -jar "%LIB_DIR%\lib\org.eclipse.osgi-${version.equinox}.jar" -clean -console -configuration "%WORK_DIR%\conf" -data "%WORK_DIR%\data"
--- runtime/org.argeo.slc.launcher/src/main/base/launch.sh
+++ runtime/org.argeo.slc.launcher/src/main/base/launch.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 INSTANCE=$1
 
+ROOT_DIR=`pwd`
+LIB_DIR=$ROOT_DIR/lib
+
 # Defaults
 if [ -z "$INSTANCE" ]; then
 	INSTANCE=agent
@@ -8,13 +11,14 @@ fi
 
 echo "Using configuration: $INSTANCE" 
 
-WORK_DIR=work/$INSTANCE
+WORK_DIR=$ROOT_DIR/work/$INSTANCE
 if [ -z "$JAVA_OPTS" ]; then
 	JAVA_OPTS=-Xmx256m
 fi
 
 if [ -d "$WORK_DIR" ]; then
-java $JAVA_OPTS -jar lib/org.eclipse.osgi-${version.equinox}.jar \
+cd $WORK_DIR
+java $JAVA_OPTS -jar $LIB_DIR/org.eclipse.osgi-${version.equinox}.jar \
 	-clean -console \
 	-configuration $WORK_DIR/conf \
 	-data $WORK_DIR/data
