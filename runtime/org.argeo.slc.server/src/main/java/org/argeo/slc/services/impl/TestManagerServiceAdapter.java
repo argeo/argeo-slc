d4278e24d7b32783990b585ed3e52a74174ec95b
Mathieu Baudier <mbaudier@argeo.org>
2010-09-20 09:32:50 +0000

    Cleanify
    git-svn-id: https://svn.argeo.org/slc/trunk@3776 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- runtime/org.argeo.slc.server/src/main/java/org/argeo/slc/services/impl/TestManagerServiceAdapter.java
+++ runtime/org.argeo.slc.server/src/main/java/org/argeo/slc/services/impl/TestManagerServiceAdapter.java
@@ -1,7 +1,5 @@
 package org.argeo.slc.services.impl;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.argeo.slc.core.attachment.Attachment;
 import org.argeo.slc.core.attachment.SimpleAttachment;
 import org.argeo.slc.core.test.tree.TreeTestResult;
@@ -13,10 +11,8 @@ import org.argeo.slc.msg.test.tree.ResultPartRequest;
 import org.argeo.slc.services.TestManagerService;
 import org.argeo.slc.test.TestResultPart;
 
+/** In memory access to a test manager service */
 public class TestManagerServiceAdapter implements TreeTestResultListener {
-
-	private final Log log = LogFactory.getLog(getClass());
-
 	private Boolean onlyOnClose = false;
 
 	private TestManagerService testManagerService;
@@ -31,19 +27,9 @@ public class TestManagerServiceAdapter implements TreeTestResultListener {
 						.getParts().size() == 1) {
 			CreateTreeTestResultRequest req = new CreateTreeTestResultRequest(
 					testResult);
-
-			if (log.isTraceEnabled())
-				log.trace("Send create result request for result "
-						+ testResult.getUuid());
-
 			testManagerService.createTreeTestResult(req);
 		} else {
 			ResultPartRequest req = new ResultPartRequest(testResult);
-
-			if (log.isTraceEnabled())
-				log.trace("Send result parts for result "
-						+ testResult.getUuid());
-
 			testManagerService.addResultPart(req);
 		}
 	}
@@ -52,29 +38,17 @@ public class TestManagerServiceAdapter implements TreeTestResultListener {
 		if (onlyOnClose) {
 			CreateTreeTestResultRequest req = new CreateTreeTestResultRequest(
 					testResult);
-
-			if (log.isTraceEnabled())
-				log.trace("Send onClose create result request for result "
-						+ testResult.getUuid());
-
 			testManagerService.createTreeTestResult(req);
 		} else {
 			CloseTreeTestResultRequest req = new CloseTreeTestResultRequest(
 					testResult);
-
-			if (log.isTraceEnabled())
-				log.trace("Send close result request for result "
-						+ testResult.getUuid());
-
 			testManagerService.closeTreeTestResult(req);
-
 		}
 	}
 
 	public void addAttachment(TreeTestResult testResult, Attachment attachment) {
 		if (onlyOnClose)
 			return;
-
 		AddTreeTestResultAttachmentRequest req = new AddTreeTestResultAttachmentRequest();
 		req.setResultUuid(testResult.getUuid());
 		req.setAttachment((SimpleAttachment) attachment);
