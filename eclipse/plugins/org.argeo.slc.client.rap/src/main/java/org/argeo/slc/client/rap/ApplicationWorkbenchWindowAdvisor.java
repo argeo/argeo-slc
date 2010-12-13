868df693349bc9216260476abdc0401d8d63e855
Bruno Sinou <bsinou@argeo.org>
2010-12-13 15:54:07 +0000

    Add a meta bundle for SLC to be used with RAP
    git-svn-id: https://svn.argeo.org/slc/trunk@3974 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ eclipse/plugins/org.argeo.slc.client.rap/src/main/java/org/argeo/slc/client/rap/ApplicationWorkbenchWindowAdvisor.java
@@ -0,0 +1,30 @@
+package org.argeo.slc.client.rap;
+
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+/**
+ * Configures the initial size and appearance of a workbench window.
+ */
+public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
+
+	public ApplicationWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		super(configurer);
+	}
+
+	public ActionBarAdvisor createActionBarAdvisor(
+			IActionBarConfigurer configurer) {
+		return new ApplicationActionBarAdvisor(configurer);
+	}
+
+	public void preWindowOpen() {
+		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+		configurer.setInitialSize(new Point(1200, 900));
+		configurer.setShowCoolBar(false);
+		configurer.setShowStatusLine(false);
+	}
+}
---
+++ eclipse/plugins/org.argeo.slc.client.rap/src/main/java/org/argeo/slc/client/rap/Perspective.java
@@ -0,0 +1,14 @@
+package org.argeo.slc.client.rap;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+/**
+ * Configures the perspective layout. This class is contributed 
+ * through the plugin.xml.
+ */
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
