c2612872b9510eb8d0fa6bcc55442fbe28bdc5a1
Mathieu Baudier <mbaudier@argeo.org>
2010-12-15 20:34:06 +0000

    Introduce distribution perspective
    git-svn-id: https://svn.argeo.org/slc/trunk@3985 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

---
+++ eclipse/plugins/runtime/org.argeo.slc.client.ui.dist/src/main/java/org/argeo/slc/client/ui/dist/DistributionPerspective.java
@@ -0,0 +1,13 @@
+package org.argeo.slc.client.ui.dist;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class DistributionPerspective  implements IPerspectiveFactory{
+
+	public void createInitialLayout(IPageLayout layout) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
