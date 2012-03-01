f5619266b1acabf1cc375487818c3d6a6e0614dc
Mathieu Baudier <mbaudier@argeo.org>
2012-03-01 21:07:55 +0000

    Remove unused stuff
    git-svn-id: https://svn.argeo.org/slc/trunk@5126 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- server/org.argeo.slc.ria/src/argeo-ria-lib/jcr/class/org/argeo/jcr/ria/provider/INodeProvider.js
+++
@@ -1,6 +0,0 @@
-qx.Interface.define("org.argeo.jcr.ria.provider.INodeProvider", {
-	members : {
-		initProvider : function(properties){},
-		loadNode : function(path, depth, filter){}
-	}
-});
--- server/org.argeo.slc.ria/src/argeo-ria-lib/slc/class/org/argeo/slc/ria/__init__.js
+++
@@ -1,5 +0,0 @@
-/**
- * Package containing SLC generic components to be used by any RIA SLC applications
- * as a library.
- * Currently contains the SlcApi client and a canonical Applet for displaying test results.
- */
