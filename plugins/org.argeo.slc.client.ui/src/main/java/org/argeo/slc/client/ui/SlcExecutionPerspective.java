3c158e1fdf8d885eaa2ece409c4cf2584839f633
Mathieu Baudier <mbaudier@argeo.org>
2013-01-13 16:23:08 +0000

    Clean up perspective. Add commented out SWT Tools Sleak view
    git-svn-id: https://svn.argeo.org/slc/trunk@6021 4cfe0d0a-d680-48aa-b62c-e0a02a3f76cc

--- plugins/org.argeo.slc.client.ui/src/main/java/org/argeo/slc/client/ui/SlcExecutionPerspective.java
+++ plugins/org.argeo.slc.client.ui/src/main/java/org/argeo/slc/client/ui/SlcExecutionPerspective.java
@@ -19,6 +19,7 @@ import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
+/** Perspective to manage SLC execution flows. */
 public class SlcExecutionPerspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout layout) {
@@ -26,34 +27,16 @@ public class SlcExecutionPerspective implements IPerspectiveFactory {
 		layout.setEditorAreaVisible(true);
 		layout.setFixed(false);
 
-		// Create the main ui layout
-
-		// For a vertical split, the part on top gets the specified ratio of the
-		// current space and the part on bottom gets the rest. Likewise, for a
-		// horizontal split, the part at left gets the specified ratio of the
-		// current space.
-		// IFolderLayout main = layout.createFolder("main", IPageLayout.RIGHT,
-		// 0.3f, editorArea);
 		IFolderLayout left = layout.createFolder("left", IPageLayout.LEFT,
 				0.3f, editorArea);
+		left.addView("org.argeo.slc.client.ui.jcrExecutionModulesView");
+		left.addView("org.argeo.slc.client.ui.jcrResultListView");
+		// Sleak view for SWT resource debugging
+		//left.addView("org.eclipse.swt.tools.views.SleakView");
 
 		IFolderLayout bottom = layout.createFolder("bottom",
 				IPageLayout.BOTTOM, 0.65f, editorArea);
 		bottom.addView("org.argeo.security.ui.logView");
 		bottom.addView("org.argeo.slc.client.ui.jcrProcessListView");
-
-		// IFolderLayout topRight = layout.createFolder("topRight",
-		// IPageLayout.RIGHT, 0.6f, "main");
-
-		// add the views to the corresponding place holder
-		left.addView("org.argeo.slc.client.ui.jcrExecutionModulesView");
-		left.addView("org.argeo.slc.client.ui.jcrResultListView");
-
-		// main.addView("org.argeo.slc.client.ui.processBuilderView");
-		// main.addPlaceholder("org.argeo.slc.client.ui.resultDetailView:UUID-*");
-		// main.addPlaceholder("org.argeo.slc.client.ui.processDetailView:UUID-*");
-
-		// topRight.addView("org.argeo.slc.client.ui.processParametersView");
 	}
-
 }
