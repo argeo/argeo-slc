f95ad6fb552996355c2faadc537e4e6dc6a9584b
Mathieu Baudier <mbaudier@argeo.org>
2020-10-16 10:52:47 +0200

    Adapt to changes in Argeo Commons.

--- rcp/org.argeo.eclipse.ui.rcp/src/org/argeo/eclipse/ui/specific/EclipseUiSpecificUtils.java
+++ rcp/org.argeo.eclipse.ui.rcp/src/org/argeo/eclipse/ui/specific/EclipseUiSpecificUtils.java
@@ -3,9 +3,24 @@ package org.argeo.eclipse.ui.specific;
 import org.eclipse.jface.viewers.ColumnViewer;
 import org.eclipse.jface.viewers.ColumnViewerToolTipSupport;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.widgets.Widget;
 
 /** Static utilities to bridge differences between RCP and RAP */
 public class EclipseUiSpecificUtils {
+	private final static String CSS_CLASS = "org.eclipse.e4.ui.css.CssClassName";
+
+	public static void setStyleData(Widget widget, Object data) {
+		widget.setData(CSS_CLASS, data);
+	}
+
+	public static Object getStyleData(Widget widget) {
+		return widget.getData(CSS_CLASS);
+	}
+
+	public static void setMarkupData(Widget widget) {
+		// does nothing
+	}
+
 	/**
 	 * TootlTip support is supported for {@link ColumnViewer} in RCP
 	 * 
